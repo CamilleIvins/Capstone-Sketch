7d6e45fb8175425892fb423fed280e2c7d13a5f1
Camille Ivins <camillemivins@gmail.com>
2023-09-26 14:01:45 -0600

    qwerty

--- Capstone-Sketch.client/src/pages/HomePage.vue
+++ Capstone-Sketch.client/src/pages/HomePage.vue
@@ -1,4 +1,21 @@
 <template>
+  <style>
+    body.debug .container,
+    body.debug .container-fluid {
+      outline: 2px double blue;
+      outline-offset: -2px;
+    }
+
+    body.debug .row {
+      outline: 2px dashed red;
+      outline-offset: -2px;
+    }
+
+    body.debug [class*="col-"] {
+      outline: 2px dotted forestgreen;
+      outline-offset: -3px;
+    }
+  </style>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
     <div class="home-card p-5 bg-white rounded elevation-3">
       <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
