5d982dfb3ed9d5e8e70c97830b79b6487a7e75f6
Camille Ivins <camillemivins@gmail.com>
2023-09-26 22:23:31 -0600

    homepage done

--- Capstone-Sketch.client/src/pages/HomePage.vue
+++ Capstone-Sketch.client/src/pages/HomePage.vue
@@ -1,63 +1,52 @@
 <template>
-  <!-- <style>
-    main.debug .container,
-    main.debug .container-fluid {
-      outline: 2px double blue;
-      outline-offset: -2px;
-    }
-
-    main.debug .row {
-      outline: 2px dashed red;
-      outline-offset: -2px;
-    }
-
-    main.debug [class*="col-"] {
-      outline: 2px dotted forestgreen;
-      outline-offset: -3px;
-    }
-  </style> -->
-  <!-- <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
-  </div> -->
-
 
   <section class="container">
     <!-- <img src="../assets/img/Geo-Stache-logo.png" style="height: 800px;" alt=""> -->
     <!-- logo and map that routes to map page -->
    
-      <section class="row justify-content-around my-2">
-        <div class="col-5">
+      <section class="row justify-md-content-around justify-content-center my-1">
+        <div class="col-md-5 col-4 order-md-0 order-1">
           <img class="logo" src="../assets/img/GeoStache.png" alt="">
 
         </div>
-        <div class="col-6 position-relative p-0">
+        <div class="col-md-7 col-12 position-relative p-0 my-md-0 my-2 map-link order-md-1 order-0">
           <!-- Map/link to map page -->
-          <img class="map-link" src="https://c1.wallpaperflare.com/preview/97/283/810/boise-idaho-overhead-view-public-domain.jpg" alt="">
+          <img class="map-pic" src="https://c1.wallpaperflare.com/preview/97/283/810/boise-idaho-overhead-view-public-domain.jpg" alt="">
           <div class="frosted-card">
-            <h1 class="ps-2">Start your search <span class="mdi mdi-arrow-down-right-bold text-light"></span></h1>
+            <h1 class="ps-2 position-relative">Start your search</h1>
           </div>
         </div>
       </section>
 <!-- photo and stache chart with links to popular staches -->
+<div class="row justify-content-center">
+  <button class="btn create my-3 col-8 col-md-4 fs-3">
+    Post a Stache
+  </button>
+</div>
       <section class="row justify-content-around my-2">
 
-        <div class="col-6 p-0">
-          <div class="row justify-content-center">
-            <button class="btn create my-3 col-8 col-md-7">
-              Post a Stache
-            </button>
-          </div>
-        <img class="homepage-pic" src="https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/topeka/Geocache_Lego_guy_ab282473-4f3c-43cf-ad96-5b676481fb3c.jpg" alt="">
-
+        <div class="col-md-6 col-12 p-0">
+          <img class="homepage-pic px-0" src="https://assets.simpleviewinc.com/simpleview/image/upload/c_limit,h_1200,q_75,w_1200/v1/clients/topeka/Geocache_Lego_guy_ab282473-4f3c-43cf-ad96-5b676481fb3c.jpg" alt="">
         </div>
-        <div class="col-5">
-          Stache chart
+
+        <div class="col-md-5 col-12 glass-card my-md-0 my-2">
+          <table class="row">
+  <tr class="col-8">
+    <th class="col-2 px-1">Company</th>
+    <th class="col-7 px-1">Contact</th>
+    <th class="col-3 px-1">Country</th>
+  </tr>
+  <tr>
+    <td class="col-2 px-1">Alfreds Futterkiste</td>
+    <td class="col-7 px-1">Maria Anders</td>
+    <td class="col-3 px-1">Germany</td>
+  </tr>
+  <tr>
+    <td class="col-2 px-1">Centro comercial Moctezuma</td>
+    <td class="col-7 px-1">Francisco Chang</td>
+    <td class="col-3 px-1">Mexico</td>
+  </tr>
+</table>
 
         </div>
       </section>
@@ -92,31 +81,61 @@ export default {
     }
   }
 }
+// show up on this size and smaller
+@media screen and (max-width: 768px) {
+.logo{
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
 
+.map-link{
+  img{
+  width: 100%;
+  height: 70vh !important;
+  border-radius: 10px;
+  object-fit: cover;
+object-position: center;
+border-radius: 0px !important;
+}
+
+box-shadow: 0 3px 3px -1px rgba(43, 43, 43, 0.85), 
+  0 5px 6px 0 rgba(43, 43, 43, 0.79), 
+  0 1px 8px 0 rgba(43, 43, 43, 0.79);
+}
+.map-link:hover{
+transform: scale(1) !important;
+transition: ease .35s;
+filter: brightness(1.2);
+}
+}
 .logo{
   max-width: 100%;
   // filter:drop-shadow (0 0 0.75rem crimson)
   filter: drop-shadow(3px 8px 7px #292828);
 }
 .map-link{
-  border-radius: 10px;
+  img{
   width: 100%;
+  height: 100%;
+  border-radius: 10px;
   object-fit: cover;
 object-position: center;
+border-radius: 10px;
+}
+
 box-shadow: 0 3px 3px -1px rgba(43, 43, 43, 0.85), 
   0 5px 6px 0 rgba(43, 43, 43, 0.79), 
   0 1px 8px 0 rgba(43, 43, 43, 0.79);
 }
 .map-link:hover{
   border-radius: 10px;
-  width: 100%;
-  object-fit: cover;
-object-position: center;
+
 transform: scale(1.03);
 transition: ease .35s;
 box-shadow: 0 0 10px 3px rgba(205, 205, 205, 0.85),
 inset 0 0 5px 5px rgba(205, 205, 205, 1);
   // 0 5px 6px 0 rgba(205, 205, 205, 0.79)
+  overflow-x: clip;
 }
 
 .frosted-card{
@@ -148,6 +167,11 @@ object-position: center;
 box-shadow: 0 3px 3px -1px rgba(43, 43, 43, 0.85), 
   0 5px 6px 0 rgba(43, 43, 43, 0.79), 
   0 1px 8px 0 rgba(43, 43, 43, 0.79);
+}
+
+.glass-card{
+  border-radius: 10px;
+  backdrop-filter: blur(3px);
 }
 .container,.container-fluid {
       outline: 2px double blue;
