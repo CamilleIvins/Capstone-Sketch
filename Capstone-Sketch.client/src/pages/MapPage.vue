1dc5f894f03463da27078ea76a9407f1a61c51b5
Camille Ivins <camillemivins@gmail.com>
2023-09-27 16:05:27 -0600

    showuserloc issue

--- Capstone-Sketch.client/src/pages/MapPage.vue
+++ Capstone-Sketch.client/src/pages/MapPage.vue
@@ -1,29 +1,28 @@
 <template>
-  <section class="container">
+  <div class="container">
     <section class="row justify-content-center">
       <!-- <div class="col-6 bg-primary">
 qwerty
     </div> -->
-    <div class="col-6 bg-danger">
-<form action class="row">
-  <!-- maxLength = Mick-proof this -->
-  <input type="text" placeholder="address" class="col-10">
+      <div class="col-6 bg-danger">
+        <form action class="row">
+          <!-- maxLength = Mick-proof this -->
+          <input type="text" placeholder="address" v-model="address" class="col-10">
 
-<i @click="locateButtonPressed()" class="col-1 mdi mdi-map-marker-outline"></i>
+          <i @click="locateButtonPressed()" class="col-1 mdi mdi-map-marker-outline"></i>
 
-</form>
-    </div>
+        </form>
+      </div>
     </section>
-    
 
-  </section>
+    <section id="map">
 
+    </section>
 
-
-
-
+  </div>
 </template>
 <!-- video # 7 has error spin wheel rendering -->
+<!-- video # 9 position absolute/relative -->
 
 <script>
 import axios, { Axios } from 'axios';
@@ -31,46 +30,71 @@ import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
 import { logger } from '../utils/Logger.js';
 export default {
-  setup(){
-  return { 
 
-    async locateButtonPressed(){
-
-      if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          // this.getAddressFrom(position.coords.latitude, position.coords.longitude)
-          logger.log(position.coords.latitude);
-          logger.log(position.coords.longitude);
-        },
-        error => {
-          logger.log(error)
-        },
-      );
-      } else {
-        logger.log("Your browser does not support the geolocation API")
-      }
-    },
-    async getAddressFrom(lat, long) {
-    axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&key=AIzaSyBifxFAXD3ecZoO52GpjV-STjO1LB1NnRg").then(response => {
-      if(response.data.error_message){
-        logger.log(response.data.error_message)
-      } else {
-        logger.log(response.data.results[0].formatted_address);
-      }
-    })
+  data() {
+    return {
+      address: ""
     }
+  },
+  setup() {
+    return {
+
+      async locateButtonPressed() {
+
+        if (navigator.geolocation) {
+          const permissions = await navigator.permissions.query({ name: 'geolocation' })
+          if (permissions.state == 'denied') {
+            return console.warn('invalid permissions')
+          }
+          navigator.geolocation.getCurrentPosition(
+            position => {
+              this.getAddressFrom(position.coords.latitude, position.coords.longitude)
+              logger.log(position.coords.latitude);
+              logger.log(position.coords.longitude);
+
+              this.showUserLocationOnTheMap(position.coords.latitude, position.coords.longitude)
+            },
+            error => {
+              logger.log(error)
+            },
+          );
+        } else {
+          logger.log("Your browser does not support the geolocation API")
+        }
+      },
+      async getAddressFrom(lat, long) {
+        axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&key=AIzaSyBifxFAXD3ecZoO52GpjV-STjO1LB1NnRg").then(response => {
+          if (response.data.error_message) {
+            logger.log(response.data.error_message)
+          } else {
+            this.address = response.data.results[0].formatted_address
+            // [0] most precise input based on long/lat address, goes to general Idaho, USA
+            logger.log(response.data.results[0].formatted_address);
+          }
+        })
+      }
 
 
-   }
+
+    },
+      showUserLocationOnTheMap(latitude, longitude) {
+    }
   }
 };
 </script>
 
 
 <style lang="scss" scoped>
-button{
+button {
   background-color: chocolate;
   color: whitesmoke;
+}
+
+#map {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 }
 </style>
