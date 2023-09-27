e024224144987464908bab3ab26c3ff7af347e36
Camille Ivins <camillemivins@gmail.com>
2023-09-27 15:20:51 -0600

    geolocating - prob hit

--- Capstone-Sketch.client/src/pages/MapPage.vue
+++ Capstone-Sketch.client/src/pages/MapPage.vue
@@ -1,57 +1,76 @@
 <template>
-   <h1>
-    User Location Page
-   </h1>
+  <section class="container">
+    <section class="row justify-content-center">
+      <!-- <div class="col-6 bg-primary">
+qwerty
+    </div> -->
+    <div class="col-6 bg-danger">
+<form action class="row">
+  <!-- maxLength = Mick-proof this -->
+  <input type="text" placeholder="address" class="col-10">
 
-  <!-- <head>
-    <title>Simple Marker</title>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBifxFAXD3ecZoO52GpjV-STjO1LB1NnRg&callback=initMap&libraries=&v=weekly" defer></script>
-  </head>
-  <body>
-    <div id="gmp-map"></div>
-  </body> -->
+<i @click="locateButtonPressed()" class="col-1 mdi mdi-map-marker-outline"></i>
+
+</form>
+    </div>
+    </section>
+    
+
+  </section>
+
+
+
+
+
 </template>
-    <script>
+<!-- video # 7 has error spin wheel rendering -->
+
+<script>
+import axios, { Axios } from 'axios';
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+import { logger } from '../utils/Logger.js';
 export default {
-  setup() {
-    return {
+  setup(){
+  return { 
 
+    async locateButtonPressed(){
+
+      if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          // this.getAddressFrom(position.coords.latitude, position.coords.longitude)
+          logger.log(position.coords.latitude);
+          logger.log(position.coords.longitude);
+        },
+        error => {
+          logger.log(error)
+        },
+      );
+      } else {
+        logger.log("Your browser does not support the geolocation API")
+      }
+    },
+    async getAddressFrom(lat, long) {
+    axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&key=AIzaSyBifxFAXD3ecZoO52GpjV-STjO1LB1NnRg").then(response => {
+      if(response.data.error_message){
+        logger.log(response.data.error_message)
+      } else {
+        logger.log(response.data.results[0].formatted_address);
+      }
+    })
     }
-  }
-}
 
-    //   "use strict";
-    //   function initMap() {
-    //     const myLatLng = {
-    //       lat: 43.60671615600586,
-    //       lng: -116.28668975830078
-    //     };
-    //     const map = new google.maps.Map(document.getElementById("gmp-map"), {
-    //       zoom: 14,
-    //       center: myLatLng,
-    //       fullscreenControl: false,
-    //       zoomControl: true,
-    //       streetViewControl: false
-    //     });
-    //     new google.maps.Marker({
-    //       position: myLatLng,
-    //       map,
-    //       title: "My location"
-    //     });
-    //   }
-    </script>
-          <style>
-            /* Always set the map height explicitly to define the size of the div
-             * element that contains the map. */
-            #gmp-map {
-              height: 100%;
-            }
-            /* Optional: Makes the sample page fill the window. */
-            html,
-            body {
-              height: 100%;
-              margin: 0;
-              padding: 0;
-            }
-          </style>
+
+   }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+button{
+  background-color: chocolate;
+  color: whitesmoke;
+}
+</style>
